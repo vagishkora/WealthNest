7fd8c136f011a976d535faba56404afbbdd7adf9
vagishkora <vagishkora@gmai.com>
2026-01-18 12:01:22 +0530

    Remove temporary upload folder

--- WealthNest_Upload/app/actions/stocks.ts
+++
@@ -1,9 +0,0 @@
-
-'use server'
-
-import { searchStocks } from "@/lib/stocks"
-
-export async function searchStocksAction(query: string) {
-    if (!query || query.length < 2) return []
-    return await searchStocks(query)
-}
